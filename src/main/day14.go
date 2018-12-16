dc0a9b7ecfb79c4402d8f82b81e74ee8c5dabac0
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2018-12-16 11:17:50 +0100

    part b, ugly AF, but working.

--- src/main/day14.go
+++ src/main/day14.go
@@ -14,15 +14,13 @@ func main() {
 	e1 := 0
 	e2 := 1
 	numRecipes := "190221"
-	endchar := numRecipes[len(numRecipes)-1]
-//	numRecipes := "59414"
 	recipesStr := make([]byte, 2, 1000000)
 	recipesStr[0] = '3'
 	recipesStr[1] = '7'
 
 	iter := 0
 
-	for {
+	for i := 0; i<20000000; i++{
 		sumRec := recipes[e1] + recipes[e2]
 		if sumRec < 10 {
 			recipes = append(recipes, sumRec)
@@ -30,19 +28,11 @@ func main() {
 			recipes = append(recipes, 1, sumRec-10)
 		}
 		recipesStr = append(recipesStr, []byte(strconv.FormatInt(int64(sumRec), 10))...)
-		if recipesStr[len(recipesStr) - 1] == endchar {
-			if strings.HasSuffix(string(recipesStr), numRecipes) || strings.HasSuffix(string(recipesStr[:len(recipesStr)-1]), numRecipes) {
-				break
-			}
-		}
 		e1 = (e1 + 1 + recipes[e1])%len(recipes)
 		e2 = (e2 + 1 + recipes[e2])%len(recipes)
 		iter++
-		if iter % 10000 == 0 {
-			fmt.Println(iter, len(recipesStr))
-		}
 	}
 
-	fmt.Println(len(recipesStr) - len(numRecipes))
+	fmt.Println(strings.Index(string(recipesStr), numRecipes))
 
 }
